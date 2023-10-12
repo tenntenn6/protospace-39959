d95052a42d9ffbeaf28d04045afae52a04bd41ce
tenntenn6 <jbbcce@gmail.com>
2023-10-12 18:24:34 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,63 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototypes = Prototype.new
+  end
+
+  def create
+    @prototypes =Prototype.new(prototype_params)
+    if @prototypes.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+
+    
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+end
+
+
